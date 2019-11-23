3e3bfea9cd1e1882c634e93288beef204e06bb53
bravery <2472937751@qq.com>
2019-11-23 16:55:14 +0800

    #饼图画好

--- src/main/java/com/gdufe/dbmsapp/controller/LoginController.java
+++ src/main/java/com/gdufe/dbmsapp/controller/LoginController.java
@@ -1,9 +1,26 @@
 package com.gdufe.dbmsapp.controller;
 
+import org.jfree.chart.ChartFactory;
+import org.jfree.chart.ChartUtilities;
+import org.jfree.chart.JFreeChart;
+import org.jfree.chart.labels.StandardPieSectionLabelGenerator;
+import org.jfree.chart.plot.PiePlot;
+import org.jfree.chart.servlet.ServletUtilities;
+import org.jfree.chart.title.TextTitle;
+import org.jfree.data.general.DefaultPieDataset;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+import java.awt.*;
+import java.io.FileOutputStream;
 import java.sql.*;
+import java.text.DecimalFormat;
+import java.text.NumberFormat;
 
 /**
  * @author: Bravery
@@ -15,6 +32,7 @@ public class LoginController {
 
     /**
      * 登录
+     *
      * @param number
      * @param password
      * @param m
@@ -65,17 +83,17 @@ public class LoginController {
         try {
             Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
         } catch (ClassNotFoundException event) {
-            html="连接数据库错误-1";
+            html = "连接数据库错误-1";
         }
         try {
             con = DriverManager.getConnection("jdbc:sqlserver://182.254.201.74:1433;DatabaseName=SQLS2345", "sa", "Py123456");
             sql = con.createStatement();
             String condition = "INSERT INTO 注册登录 VALUES('" + number + "','" + password + "')";
             sql.executeUpdate(condition); //执行添加操作：
-            html ="注册成功，可以利用该账号登录系统啦。";
+            html = "注册成功，可以利用该账号登录系统啦。";
             con.close();
         } catch (SQLException event) {
-            html="访问数据库错误-2，该学号已经被注册或数据填写太多。";
+            html = "访问数据库错误-2，该学号已经被注册或数据填写太多。";
         }
         m.addAttribute("html", html);
         return "register";
@@ -83,91 +101,114 @@ public class LoginController {
     }
 
 
-
     /**
      * 饼图数据统计
-     * @param t1
-     * @param m
+     *
      * @return
      */
-    @PostMapping("/piechartbyxf")
-    public String caculate(String t1, Model m){
-//        Connection con=null;
-//        Statement sql=null;
-//        ResultSet rs=null;
-//        try{Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");}
-//        catch(ClassNotFoundException e){out.print("连接数据库错误-1");}
-//        try {
-//            con=DriverManager.getConnection("jdbc:sqlserver://182.254.201.74:1433;DatabaseName=SQLS2345","sa","admins");
-//            sql=con.createStatement(ResultSet.TYPE_SCROLL_SENSITIVE, ResultSet.CONCUR_READ_ONLY);
-//            String condition="select count(课程类别) from 毕业学分";
-//            rs=sql.executeQuery(condition);
-//            int nn=1;
-//            while(rs.next()){
-//                nn=rs.getInt(1);
-//            }
-//
-//            condition="select count(课程类别) from 毕业学分  group by 课程类别";
-//            rs=sql.executeQuery(condition);
-//            int m1=0;
-//            while(rs.next()){
-//                m1=m1+1;
-//            }
-//            String[] tlTitle=new String[m1];//动态声明数组：用于存储图例标题
-//            double[] tldatas=new double[m1];//动态声明数组：用于存储图例标题对应的数据
-//            condition="select 课程类别,SUM(最低学分) from 毕业学分  group by 课程类别";
-//            rs=sql.executeQuery(condition);
-//            int i=0;
-//            while(rs.next()){
-//                tlTitle[i]=rs.getString(1);//更新图例标题
-//                tldatas[i]=rs.getInt(2)*1.0/nn;   //更新图例标题对应的数据
-//                i++;
-//            }
-//            con.close();
-//
-//            //设置数据集
-//            DefaultPieDataset dataset = new DefaultPieDataset();
-//            for(i=0;i<m1;i++)
-//            { dataset.setValue(tlTitle[i],tldatas[i]);}
-//
-//            //通过工厂类生成JFreeChart对象
-//            JFreeChart chart = ChartFactory.createPieChart3D("毕业学分表数据的统计处理",
-//                    dataset, true, false, false);
-//
-//            PiePlot pieplot = (PiePlot) chart.getPlot();
-//            DecimalFormat df = new DecimalFormat("0.00%");//获得一个DecimalFormat对象，主要是设置小数问题
-//            NumberFormat nf = NumberFormat.getNumberInstance();//获得一个NumberFormat对象
-//            StandardPieSectionLabelGenerator sp = new StandardPieSectionLabelGenerator(
-//                    "{0}{2}", nf, df);//获得StandardPieSectionLabelGenerator对象
-//            pieplot.setLabelGenerator(sp);//设置饼图显示百分比
-//
-//            //没有数据的时候显示的内容
-//            pieplot.setNoDataMessage("无数据显示");
-//            pieplot.setCircular(false);
-//            pieplot.setLabelGap(0.02D);
-//
-//            pieplot.setIgnoreNullValues(true);//设置不显示空值
-//            pieplot.setIgnoreZeroValues(true);//设置不显示负值
-//
-//            //标题文字乱码  IT行业职业分布图
-//            TextTitle textTitle = chart.getTitle();
-//            textTitle.setFont(new Font("宋体", Font.PLAIN, 20));
-//
-//            //饼上的文字乱码
-//            PiePlot plot = (PiePlot) chart.getPlot();
-//            plot.setLabelFont(new Font("宋体", Font.PLAIN, 12));
-//
-//            //图例文字乱码 饼图下面的5个说明
-//            chart.getLegend().setItemFont(new Font("宋体", Font.PLAIN, 12));
-//
+    @GetMapping("/piechartbyxf")
+    public ModelAndView caculate(ModelAndView m, HttpServletRequest request, HttpSession session) {
+        Connection con = null;
+        Statement sql = null;
+        ResultSet rs = null;
+        StringBuffer html = new StringBuffer();
+        try {
+            Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+        } catch (ClassNotFoundException e) {
+//            html = "连接数据库错误-1";
+        }
+        try {
+            con = DriverManager.getConnection("jdbc:sqlserver://182.254.201.74:1433;DatabaseName=SQLS2345", "sa", "Py123456");
+            sql = con.createStatement(ResultSet.TYPE_SCROLL_SENSITIVE, ResultSet.CONCUR_READ_ONLY);
+            String condition = "select count(课程类别) from 专业计划";
+            rs = sql.executeQuery(condition);
+            int nn = 1;
+            while (rs.next()) {
+                nn = rs.getInt(1);
+            }
+
+            condition = "select count(课程类别) from 专业计划  group by 课程类别";
+            rs = sql.executeQuery(condition);
+            int m1 = 0;
+            while (rs.next()) {
+                m1 = m1 + 1;
+            }
+            String[] tlTitle = new String[m1];//动态声明数组：用于存储图例标题
+            double[] tldatas = new double[m1];//动态声明数组：用于存储图例标题对应的数据
+            condition = "select 课程类别,SUM(最低学分) from 专业计划  group by 课程类别";
+            rs = sql.executeQuery(condition);
+            int i = 0;
+            while (rs.next()) {
+                tlTitle[i] = rs.getString(1);//更新图例标题
+                tldatas[i] = rs.getInt(2) * 1.0 / nn;   //更新图例标题对应的数据
+                i++;
+            }
+            con.close();
+
+            //设置数据集
+            DefaultPieDataset dataset = new DefaultPieDataset();
+            for (i = 0; i < m1; i++) {
+                dataset.setValue(tlTitle[i], tldatas[i]);
+            }
+
+            //通过工厂类生成JFreeChart对象
+            JFreeChart chart = ChartFactory.createPieChart3D("毕业学分表数据的统计处理",
+                    dataset, true, false, false);
+
+            PiePlot pieplot = (PiePlot) chart.getPlot();
+            DecimalFormat df = new DecimalFormat("0.00%");//获得一个DecimalFormat对象，主要是设置小数问题
+            NumberFormat nf = NumberFormat.getNumberInstance();//获得一个NumberFormat对象
+            StandardPieSectionLabelGenerator sp = new StandardPieSectionLabelGenerator(
+                    "{0}{2}", nf, df);//获得StandardPieSectionLabelGenerator对象
+            pieplot.setLabelGenerator(sp);//设置饼图显示百分比
+
+            //没有数据的时候显示的内容
+            pieplot.setNoDataMessage("无数据显示");
+            pieplot.setCircular(false);
+            pieplot.setLabelGap(0.02D);
+
+            pieplot.setIgnoreNullValues(true);//设置不显示空值
+            pieplot.setIgnoreZeroValues(true);//设置不显示负值
+
+            //标题文字乱码  IT行业职业分布图
+            TextTitle textTitle = chart.getTitle();
+            textTitle.setFont(new Font("宋体", Font.PLAIN, 20));
+
+            //饼上的文字乱码
+            PiePlot plot = (PiePlot) chart.getPlot();
+            plot.setLabelFont(new Font("宋体", Font.PLAIN, 12));
+
+            //图例文字乱码 饼图下面的5个说明
+            chart.getLegend().setItemFont(new Font("宋体", Font.PLAIN, 12));
+
+
+            FileOutputStream fos_jpg = null;
+            try {
+                fos_jpg = new FileOutputStream("./src/main/resources/static/images/test.jpg");
+                ChartUtilities.writeChartAsJPEG(fos_jpg, 100, chart, 640, 480, null);
+            } catch (Exception e) {
+            }
+
+
 //            String filename = ServletUtilities.saveChartAsPNG(chart, 500, 300,
 //                    null, session);
-//            String graphURL = request.getContextPath()+"/servlet/DisplayChart?filename="+filename;
+//            String graphURL = request.getContextPath() + "/servlet/DisplayChart?filename=" + filename;
 //
-//            return "piechartbyxf";
-        return null;
+//            //    <body>
+//            //        <center> <img src="<%=graphURL%>" width=500 height=300 border=0 usemap="#<%= filename %>"></center>
+//            //     </body>
+//            html.append("<body>");
+//            html.append("<center> <img src=" + graphURL + "+ width=500 height=300 border=0 usemap=" + filename + "></center>");
+//            html.append("</body>");
+//            m.addObject("html", html);
+//            m.setViewName("piechartbyxf");
+            return m;
+        } catch (Exception e) {
+            System.out.println(2222);
+        }
+        m.setViewName("piechartbyxf");
+        return m;
     }
-
 
 }
 
--- src/main/webapp/WEB-INF/jsp/piechartbyxf.jsp
+++ src/main/webapp/WEB-INF/jsp/piechartbyxf.jsp
@@ -1,23 +1,10 @@
-<%@ page contentType="text/html;charset=GBK"%>
-<%@ page
-    import="org.jfree.chart.*,org.jfree.chart.plot.PiePlot,
-     org.jfree.data.general.DefaultPieDataset,
-     org.jfree.chart.servlet.ServletUtilities,
-     java.awt.*,org.jfree.chart.title.TextTitle"%>
-<%@ page
-    import="org.jfree.chart.labels.StandardPieSectionLabelGenerator"%>
-<%@ page import="java.text.NumberFormat"%>
-<%@page import="java.text.DecimalFormat"%>
-<%@ page import="java.sql.*" %>
+
 <html>
-     <head>
-         <title>毕业学分表数据的统计处理（饼图）</title>
-     </head>
-<%
-  %>
-     <body>
-        <center> <img src="<%=graphURL%>" width=500 height=300 border=0
-             usemap="#<%= filename %>"></center>
-     </body>
- <%} catch(SQLException event){ out.print("访问数据库错误-2");} %>
- </html>
+<head>
+    <title>毕业学分表数据的统计处理（饼图）</title>
+</head>
+<body>
+<center><img src="images/test.jpg" width=800 height=500</center>
+</body>
+
+</html>
